e59de26ee6eda31cd7bc7978d17538f593b12a61
jeorgithon <jeor_d@hotmail.com>
2021-02-18 23:58:23 -0300

    teste merge

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -22,6 +22,70 @@
             body {
                 font-family: 'Nunito';
             }
+            /* Fixed sidenav, full height */
+            .sidenav {
+            height: 100%;
+            width: 200px;
+            position: fixed;
+            z-index: 1;
+            top: 62px;
+            left: 0;
+            background-color: #111;
+            overflow-x: hidden;
+            padding-top: 20px;
+            }
+
+            /* Style the sidenav links and the dropdown button */
+            .sidenav a, .dropdown-btn {
+            padding: 6px 8px 6px 16px;
+            text-decoration: none;
+            font-size: 20px;
+            color: #818181;
+            display: block;
+            border: none;
+            background: none;
+            width: 100%;
+            text-align: left;
+            cursor: pointer;
+            outline: none;
+            }
+
+            /* On mouse-over */
+            .sidenav a:hover, .dropdown-btn:hover {
+            color: #f1f1f1;
+            }
+
+            /* Main content */
+            .main {
+            margin-left: 200px; /* Same as the width of the sidenav */
+            font-size: 20px; /* Increased text to enable scrolling */
+            padding: 0px 10px;
+            }
+
+            /* Add an active class to the active dropdown button */
+            .active {
+            background-color: green;
+            color: white;
+            }
+
+            /* Dropdown container (hidden by default). Optional: add a lighter background color and some left padding to change the design of the dropdown content */
+            .dropdown-container {
+            display: none;
+            background-color: #262626;
+            padding-left: 8px;
+            }
+
+            /* Optional: Style the caret down icon */
+            .fa-caret-down {
+            float: right;
+            padding-right: 8px;
+            }
+
+            /* Some media queries for responsiveness */
+            @media screen and (max-height: 450px) {
+            .sidenav {padding-top: 15px;}
+            .sidenav a {font-size: 18px;}
+            }
         </style>
     </head>
     <body>
@@ -56,9 +120,47 @@
                 </div>
             </nav>
             
-            <div class="container" style="padding-top: 10px">
+            <div class="container">
                 <div class = "row justify-content-center">
-                    <div class="col-md-2">
+                    
+                        <div class="sidenav">
+                            <button class="dropdown-btn">Militar 
+                                <i class="fa fa-caret-down"></i>
+                            </button>
+                            <div class="dropdown-container">
+                                <a href="/cadastroMilitar">Cadastrar Militar</a>
+                                <a href="/listar/militar">Listar Militar</a>
+                            </div>
+                            <button class="dropdown-btn">Viatura 
+                                <i class="fa fa-caret-down"></i>
+                            </button>
+                            <div class="dropdown-container">
+                                <a href="/cadastro/viatura">Cadastrar Viatura</a>
+                                <a href="/listar/viatura">Listar Viatura</a>
+                            </div>
+                            <button class="dropdown-btn">Guarnição 
+                                <i class="fa fa-caret-down"></i>
+                            </button>
+                            <div class="dropdown-container">
+                                <a href="/cadastro/guarnicao">Cadastrar Guarnição</a>
+                                <a href="/listar/guarnicao">Listar Guarnição</a>
+                            </div>
+                            <button class="dropdown-btn">Cidade 
+                                <i class="fa fa-caret-down"></i>
+                            </button>
+                            <div class="dropdown-container">
+                                <a href="/cadastro/cidade">Cadastrar Cidade</a>
+                                <a href="/listar/cidade">Listar Cidade</a>
+                            </div>
+                            <button class="dropdown-btn">Serviços 
+                                <i class="fa fa-caret-down"></i>
+                            </button>
+                            <div class="dropdown-container">
+                                <a href="/cadastro/servico">Cadastrar Serviços</a>
+                                <a href="/listar/servico">Listar Serviços</a>
+                            </div>
+                        </div>
+                        <!--
                         <div class = "list-group">
                             <div class="btn-group">
                             <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="width:100%">
@@ -107,12 +209,30 @@
                             </div>
 
                         </div>
-                    </div>
+                        -->
+                    
 
-                <div class="col-md-10">
+                <div class="col-md-12">
                     @yield('content')
                 </div>
             </div>
+            <script>
+            /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+            var dropdown = document.getElementsByClassName("dropdown-btn");
+            var i;
+
+            for (i = 0; i < dropdown.length; i++) {
+            dropdown[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var dropdownContent = this.nextElementSibling;
+            if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+            } else {
+            dropdownContent.style.display = "block";
+            }
+            });
+            }
+            </script>
         </div>
     </body>
 </html>
